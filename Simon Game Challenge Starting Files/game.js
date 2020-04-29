a871c6ddeb60bba58753668fe7c7099bd064dbee
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-04-29 16:20:56 +1000

    add animation

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -7,6 +7,7 @@ $(".btn").click(function(){
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour); 
     playSound(userChosenColour);
+    animatePress(userChosenColour);
 })
 
 function nextSequence() {
@@ -22,7 +23,17 @@ function nextSequence() {
 
 function playSound(name){
     var audio = new Audio ("sounds/" + name + ".mp3");
-audio.play();
+    audio.play();
 };
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+    
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+};
+
+
 
 
