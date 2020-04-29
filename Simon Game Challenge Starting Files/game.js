14590c33f5e1cb297c084924a032ecf421b8077b
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-04-29 14:23:06 +1000

    add sound

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,28 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern =[];
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour); 
+    playSound(userChosenColour);
+})
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    var randomChosenColour = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    var audio = new Audio ("sounds/" + name + ".mp3");
+audio.play();
+};
+
+
