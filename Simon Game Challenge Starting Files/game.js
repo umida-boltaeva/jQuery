f3621cce6d4127cae2a55ae7a506eada13545d00
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-05-06 13:20:57 +1000

    simon game started!

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,15 +2,40 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 var userClickedPattern =[];
+var started = false;
+var level = 0;
+
+$(document).keypress(function(){
+    if(!started){
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
 
 $(".btn").click(function(){
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour); 
     playSound(userChosenColour);
     animatePress(userChosenColour);
-})
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+        }
+    }
+};
 
 function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4);
 
     var randomChosenColour = buttonColors[randomNumber];
@@ -33,6 +58,8 @@ function animatePress(currentColour){
         $("#" + currentColour).removeClass("pressed");
     }, 100);
 };
+
+
 
 
 
